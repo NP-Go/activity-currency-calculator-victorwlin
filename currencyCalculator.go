90df6d4b1ea801b3bcb07e43c05b9cb13c9261ed
Victor Lin <linvictorw@gmail.com>
2022-03-29 22:07:51 +0800

    Add calculations

--- currencyCalculator.go
+++ currencyCalculator.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func CurrencyCalculator(oneDollar, fiftyCent, twentyCent, tenCent, fiveCent float64) (float64, float64, float64) {
 	//Insert your code here
+	totalAmount := oneDollar * 1 + fiftyCent * 0.5 + twentyCent * 0.2 + tenCent * 0.1 + fiveCent * 0.05
+	twoDollarNotes := math.Trunc(totalAmount / 2)
+	changeAmount := math.Mod(totalAmount, 2)
 
 	//Do not remove this
 	fmt.Println("Total:", totalAmount, "Two Dollar Notes:", twoDollarNotes, " Change: ", changeAmount)
@@ -19,15 +23,15 @@ func main() {
 	var tenCentCoin float64
 	var fiveCentCoin float64
 
-	fmt.Println("Enter the number of 1-dollar coins: ")
+	fmt.Printf("Enter the number of 1-dollar coins: ")
 	fmt.Scanln(&oneDollarCoin)
-	fmt.Println("Enter the number of 50-cent coins: ")
+	fmt.Printf("Enter the number of 50-cent coins: ")
 	fmt.Scanln(&fiftyCentCoin)
-	fmt.Println("Enter the number of 20-cent coins: ")
+	fmt.Printf("Enter the number of 20-cent coins: ")
 	fmt.Scanln(&twentyCentCoin)
-	fmt.Println("Enter the number of 10-cent coins: ")
+	fmt.Printf("Enter the number of 10-cent coins: ")
 	fmt.Scanln(&tenCentCoin)
-	fmt.Println("Enter the number of 5-cent coins: ")
+	fmt.Printf("Enter the number of 5-cent coins: ")
 	fmt.Scanln(&fiveCentCoin)
 	CurrencyCalculator(oneDollarCoin, fiftyCentCoin, twentyCentCoin, tenCentCoin, fiveCentCoin)
 }
