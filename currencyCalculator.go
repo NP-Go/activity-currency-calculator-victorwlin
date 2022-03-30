86f50efccf2772dd86f9d56480e63e2534a4e14e
Victor Lin <linvictorw@gmail.com>
2022-03-30 14:07:31 +0800

    Fix formatting

--- currencyCalculator.go
+++ currencyCalculator.go
@@ -7,12 +7,12 @@ import (
 
 func CurrencyCalculator(oneDollar, fiftyCent, twentyCent, tenCent, fiveCent float64) (float64, float64, float64) {
 	//Insert your code here
-	totalAmount := oneDollar * 1 + fiftyCent * 0.5 + twentyCent * 0.2 + tenCent * 0.1 + fiveCent * 0.05
+	totalAmount := oneDollar * 1.00 + fiftyCent * 0.50 + twentyCent * 0.20 + tenCent * 0.10 + fiveCent * 0.05
 	twoDollarNotes := math.Trunc(totalAmount / 2)
-	changeAmount := math.Mod(totalAmount, 2)
+	changeAmount := math.Mod(totalAmount, 2.00)
 
 	//Do not remove this
-	fmt.Println("Total:", totalAmount, "Two Dollar Notes:", twoDollarNotes, " Change: ", changeAmount)
+	fmt.Printf("Total: %.2f  Two Dollar Notes: %v  Change: %.2f", totalAmount, twoDollarNotes, changeAmount)
 	return totalAmount, twoDollarNotes, changeAmount
 }
 
